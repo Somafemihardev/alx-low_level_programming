8a70690c28bff0c12d9d403987282eaf491a18a2
SOmafemihardev <oluwasegunmathew603@gmail.com>
2023-06-16 08:36:51 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,14 @@
 int main(void)
 {
     char letter;
-    // Print lowercase alphabet
     for (letter = 'a'; letter <= 'z'; letter++)
     {
     	    putchar(letter);
     }
-    // Print uppercase alphabet
     for (letter = 'A'; letter <= 'Z'; letter++)
     {
         putchar(letter);
     }
     putchar('\n');
-    return (0)
+    return (0);
 }
